addc42af60381973c431a7fe6015280e8047c4b1
JulianaAbreu <juliana.cerqueira55@gmail.com>
2018-03-24 01:27:17 -0300

    add content of course

--- src/components/StudentList/index.js
+++ src/components/StudentList/index.js
@@ -6,33 +6,9 @@ import './style.css';
 class StudentList extends React.Component {
 
   render() {
-    const {students, deleteByIndex} = this.props; 
     return (
       <div className="card StudentList">
-        <table>
-          <thead>
-            <tr>
-              <th>Nome</th>
-              <th>Matrícula</th>
-              <th>Série</th>
-              <th>RG</th>
-            </tr>
-          </thead>
-          <tbody>
-            {students.map((student, index) => (
-              <StudentRow
-                nome={student.nome}
-                matricula={student.matricula}
-                serie={student.serie}
-                rg={student.rg}
-                key={student.rg}
-                index={index} 
-                deleteByIndex={deleteByIndex} 
-              />
-              )
-            )}
-          </tbody>
-        </table>
+       
       </div>
     );
   }
--- src/components/StudentRow/index.js
+++ src/components/StudentRow/index.js
@@ -2,21 +2,10 @@ import React from "react";
 import "./style.css";
 
 class StudentRow extends React.Component {
-  onClick = (ev) => {
-      const { deleteByIndex } = this.props;
-      ev.preventDefault();
-      deleteByIndex(this.props.index);   
-  }
   render() {
     return (
       <tr>
-        <td>{this.props.nome}</td>
-        <td>{this.props.matricula}</td>
-        <td>{this.props.serie}</td>
-        <td>{this.props.rg}</td>
-        <td>
-          <button onClick={this.onClick} className="btn-remove">×</button>
-        </td>
+      
       </tr>
     );
   }
